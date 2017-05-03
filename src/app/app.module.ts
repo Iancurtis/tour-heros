c1a144b095499c9be8876b53af575938368655e0
Fyn <fyn@bohaisoft.cn>
2017-05-03 12:55:36 +0800

    4. Multiple Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,19 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
+// <-- Every component must be declared in one—and only one—Angular module.
 import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
   imports:      [ // <-- which contains the list of external modules that the app uses
     BrowserModule,
     FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
   ],
-  declarations: [ AppComponent ],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
