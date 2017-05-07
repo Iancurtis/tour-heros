d33a08b9c6526d79adf1f6fdefbc880f35798e03
fyn <fyn@bohaisoft.cn>
2017-05-07 11:38:32 -0700

    css styles :host selector

--- src/app/attribute-directive.component.ts
+++ src/app/attribute-directive.component.ts
@@ -2,7 +2,13 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-attribute-directive',
-  templateUrl: './attribute-directive.component.html'
+  templateUrl: './attribute-directive.component.html',
+  styles: [`
+    :host {
+      display: block;
+      border: 1px solid black;
+    }
+  `],
 })
 
 export class AttributeDirectiveComponent implements OnInit {
