03c4bbbef7d1c8fb9be1aa4183ac35a20d651b6d
Fyn <fyn@bohaisoft.cn>
2017-05-08 00:06:35 +0800

    属性型指令 2 highlight数据绑定

--- src/app/attribute-directive.component.ts
+++ src/app/attribute-directive.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class AttributeDirectiveComponent implements OnInit {
+  color = 'orange';
+
   constructor() { }
 
   ngOnInit() { }
--- src/app/directive/highlight.directive.ts
+++ src/app/directive/highlight.directive.ts
@@ -1,10 +1,12 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({ selector: '[myHighlight]' })
 export class HighlightDirective {
 
+  @Input('myHighlight') highlightColor: string;
+
   @HostListener('mouseenter') onMouseEnter() {
-    this.highlight('yellow');
+    this.highlight(this.highlightColor || 'red');
   }
 
   @HostListener('mouseleave') onMouseLeave() {
